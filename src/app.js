932a8495281e894fda8a892bdfbcd53776879950
JRobbo85 <51794281+JRobbo85@users.noreply.github.com>
2022-10-19 10:57:38 +0100

    set up app and connection files

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const yargs = require("yargs")
+const {sequelize} = require("./db/connection")
+
+const app = async (yargsObject) => {
+    try {
+        await sequelize.sync()
+
+        if (yargsObject.create){
+
+        }
+        else if (yargsObject.read){
+
+        }
+        else if (yargsObject.update){
+
+        }
+        else if (yargsObject.delete){
+
+        }
+        else {
+            console.log("Incorrect Command")
+        }
+
+        await sequelize.close()
+    } 
+    catch (error) {
+        console.log(error)
+        await sequelize.close()
+    }
+}
+
+app(yargs.argv)
