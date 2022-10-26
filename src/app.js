4f39ab71028a130436137935d3daae96f08a34d6
JRobbo85 <51794281+JRobbo85@users.noreply.github.com>
2022-10-26 12:51:31 +0100

    Created all user funcs using movie as template

--- src/app.js
+++ src/app.js
@@ -1,47 +1,125 @@
 const yargs = require("yargs")
 const {sequelize} = require("./db/connection")
-const {createMovie, readMovie} = require("./movie/movieFunction")
+const {createMovie, readMovie, updateMovie, deleteMovie} = require("./movie/movieFunction")
+const {createUser, readUser, updateUser, deleteUser} = require("./users/userFuncs")
+const {faveMovie, searchMovies, searchUsers} = require
+
 
 const app = async (yargsObject) => {
     try {
         await sequelize.sync()
 
-        if (yargsObject.create){
+        if (yargsObject.createMovie){                                                                        //Create Func
             await createMovie({title: yargsObject.title, actor: yargsObject.actor})
             let output = {}
             let table = await readMovie()
             for (let movie of table){
                 output.id = movie.id
                 output.title = movie.title
-                output.actor = movie.actorconsole.log(output)
+                output.actor = movie.actor
+                console.log(output)
             }
         }
-        else if (yargsObject.read){
+        else if (yargsObject.readMovie){                                                                     //Read Func
             console.log(await readMovie({[yargsObject.key] : yargsObject.value}))
             let output = {}
             let table = await readMovie()
             for (let movie of table){
                 output.id = movie.id
                 output.title = movie.title
-                output.actor = movie.actorconsole.log(output)
+                output.actor = movie.actor
+                console.log(output)
             }
         }
-        else if (yargsObject.readAll){
+        else if (yargsObject.readAll){                                                                  //Read All Fallback Func
             console.log(await readMovie())
             let output = {}
             let table = await readMovie()
             for (let movie of table){
                 output.id = movie.id
                 output.title = movie.title
-                output.actor = movie.actorconsole.log(output)
+                output.actor = movie.actor
+                console.log(output)
             }
         }
-        else if (yargsObject.update){
+        else if (yargsObject.updateMovie){                                                              //Update Func
+            await updateMovie({title: yargsObject.title, actor: yargsObject.actor, director: yargsObject.director, user: parseInt(yargsObject.user)}, {[yargsObject.key] : [yargsObject.value]})
+            let output = {}
+            let table = await readMovie()
+            for (let movie of table) {
+                output.id = movie.id
+                output.title = movie.title
+                output.actor = movie.actor
+                output.director = movie.director
+                output.user = movie.user
+                console.log(output)
+            }}
+        
+        else if (yargsObject.deleteMovie){                                                              //Delete Func
+            await deleteMovie({ [yargsObject.key] : [yargsObject.value] })
+            let output = {}
+            let table = await readMovie()
+            for (let movie of table) {
+                output.id = movie.id
+                output.title = movie.title
+                output.actor = movie.actor
+                output.director = movie.director
+                output.user = movie.user
+                console.log(output)
+        }}
 
+        if (yargsObject.createUser){                                                                        //Create Func
+            await createUser({name: yargsObject.name, memnum: yargsObject.memnum})
+            let output = {}
+            let table = await readUser()
+            for (let user of table){
+                output.id = user.id
+                output.title = user.title
+                output.memnum = user.memnum
+                console.log(output)
+            }
         }
-        else if (yargsObject.delete){
-
+        else if (yargsObject.readUser){                                                                     //Read Func
+            let output = {}
+            let table = await readUser({ [yargsObject.key] : yargsObject.value})
+                output.id = table.id
+                output.name = table.name
+                output.memnum = table.memnum
+                console.log(output)
+            }
+        
+        else if (yargsObject.readAllUsers){                                                                  //Read All Fallback Func
+            let output = {}
+            let table = await readUser()
+            for (let user of table) {
+                output.id = user.id
+                output.name = user.name
+                output.memnum = user.memnum
+                console.log(output)
+            }
         }
+        else if (yargsObject.updateUser){                                                                   //Update Func
+            await updateUser({name: yargsObject.name, memnum: yargsObject.memnum}, { [yargsObject.key] : [yargsObject.value] })
+            let output = {}
+            let table = await readUser()
+            for (let user of table) {
+                output.id = user.id
+                output.name = user.name
+                output.memnum = user.memnum
+                console.log(output)
+            }}
+        
+        else if (yargsObject.delete){                                                                       //Delete Func
+            await deleteUser({ [yargsObject.key] : [yargsObject.value] })
+            let output = {}
+            let table = await readUser()
+            for (let user of table) {
+                output.id = user.id
+                output.name = user.name
+                output.memnum = user.memnum
+                console.log(output)
+        }}
+    
         else {
             console.log("Incorrect Command")
         }
--- src/users/userTable.js
+++ src/users/userTable.js
@@ -6,7 +6,7 @@ const User = sequelize.define("User", {
         type: DataTypes.STRING,
         allowNull: false,
     },
-    membership: {
+    memnum: {
         type: DataTypes.STRING,
         defaultValue: "Not specified",
     },
